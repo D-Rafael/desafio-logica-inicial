502d6ea06a776ed62e9a092daed71caca9a99732
D-Rafael <drs10.trab@gmail.com>
2024-08-20 20:19:32 -0300

    Desafio Nivel de Heróis Finalizado

---
+++ README.md
@@ -0,0 +1,27 @@
+# Desafio Classificador de nível de Herói
+
+## **O que deve ser utilizado**
+
+- Variaveis
+- Operadores
+- Laços
+- Estruturas de decisão
+
+### Objetivo
+
+Crie uma variavel para armazenar o nome e a quantidade de experiencia (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+
+Se XP for menor do que 1.000 = Ferro
+Se XP for entre 1.001 e 2.000 = Bronze
+Se XP for entre 2.001 e 6.000 = Prata
+Se XP for entre 6.001 e 7.000 = Ouro
+Se XP for entre 7.001 e 8.000 = Platina
+Se XP for entre 8.001 e 9.000 = Diamante
+Se XP for entre 9.001 e 10.000 = Ascendente
+Se XP for entre 10.001 e 11.000 = Imortal
+Se XP for maior ou igual 11.001 = Radiante
+
+### Saída
+
+Ao final deve se exibir uma mensagem:
+"O herói de nome **{nome}** está no nível de **{nível}**
--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("Hello World");
+let nomeNivelHeroi = [
+  ["DRS", 11980],
+  ["Aragorn", 930],
+  ["Darktru", 1500],
+  ["Drogon", 2500],
+  ["GhostReborn", 6300],
+  ["Trifeta", 7543],
+  ["fritew", 8765],
+  ["vertu", 9866],
+  ["brino", 10500],
+];
+
+function obterNivelXP(xp) {
+  if (xp <= 1000) {
+    return "você está no Ferro!!";
+  } else if (xp >= 1001 && xp <= 2000) {
+    return "você está no Bronze!!";
+  } else if (xp >= 2001 && xp <= 6000) {
+    return "você está no Prata!!";
+  } else if (xp >= 6001 && xp <= 7000) {
+    return "você está no Ouro!!";
+  } else if (xp >= 7001 && xp <= 8000) {
+    return "você está no Platina!!";
+  } else if (xp >= 8001 && xp <= 9000) {
+    return "você está no Diamante!!";
+  } else if (xp >= 9001 && xp <= 10000) {
+    return "você está no Ascendente!!";
+  } else if (xp >= 10001 && xp <= 11000) {
+    return "você está no Imortal!!";
+  } else if (xp >= 11001) {
+    return "você está no Radiante!!";
+  }
+}
+//O forEach percorre cada item de um array e executa uma função para cada elemento.
+nomeNivelHeroi.forEach((hero) => {
+  let nome = hero[0];
+  let xp = hero[1];
+  let nivel = obterNivelXP(xp);
+  console.log(`${nome} ${nivel} com ${xp} de XP.`)
+})
